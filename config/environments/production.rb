913c662a6754cf784480fcc2b549ead884b7f9a3
Yuri <yuri.manahan@gmail.com>
2017-04-03 18:06:45 -0400

    modified actionmailer for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,10 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "purplemint_website_#{Rails.env}"
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.perform_caching = false
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
