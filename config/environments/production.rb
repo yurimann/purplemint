fa5b00b64aa53de2d6984417050d97c7d65445e2
Yuri <yuri.manahan@gmail.com>
2017-04-04 15:58:36 -0400

    Add host to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,8 +55,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "purplemint_website_#{Rails.env}"
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => "purplemint.herokuapp.com" }
   config.action_mailer.perform_caching = false
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
