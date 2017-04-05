fcda6239875cd01905a337421896833d617f4c98
Yuri <yuri.manahan@gmail.com>
2017-04-05 11:49:07 -0400

    added nested fields

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,5 +2,5 @@ class Menu < ApplicationRecord
   has_and_belongs_to_many :entrees
   has_and_belongs_to_many :beverages
   has_and_belongs_to_many :desserts
-  accepts_nested_attributes_for :entrees, :desserts, :beverages
+  accepts_nested_attributes_for :entrees, :desserts, :beverages, :reject_if => :all_blank, :allow_destroy => true
 end
